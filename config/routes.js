f61c0b5dbc0673cf22f0ef02a01459784963a391
samjeffcoat <samjeffcoat@gmail.com>
2019-03-08 10:59:25 -0600

    Adds a login function

--- config/routes.js
+++ config/routes.js
@@ -38,6 +38,24 @@ function generateToken(user){
 
 function login(req, res) {
   // implement user login
+  let {username, password} = req.body;
+  Users.findBy({username})
+  .first()
+  .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)){
+      const token =generateToken(user);
+      res.status(200).json({
+        message: `Welcome ${user.username}, here is your magical token!`,
+        token,
+        secret
+      });
+    } else{
+      res.status(401).json({message: "Invalid Credentials"});
+    }
+  })
+  .catch(error => {
+    res.status(500).json(error)
+  })
 }
 
 function getJokes(req, res) {
