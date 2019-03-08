39e0a336d715ea47932451a5e4e710714f7a8f1d
samjeffcoat <samjeffcoat@gmail.com>
2019-03-08 16:08:28 -0600

    Authentication function updated

--- auth/authenticate.js
+++ auth/authenticate.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const jwt = require('jsonwebtoken');
 
 const jwtKey =
@@ -15,24 +17,19 @@ function authenticate(req, res, next) {
 
   if (token) {
     jwt.verify(token, jwtKey, (err, decoded) => {
-      if (err) return res.status(401).json(err);
-
+      console.log(token)
+      console.log(jwtKey)
+      console.log(req.decoded)
+      if (err) {
+         res.status(401).json(err);
+        }
+        else{
       req.decoded = decoded;
-
       next();
+    }
     });
   } else {
     return res.status(401).json({
-      error: 'No token provided, must be set on the Authorization Header',
-    });
+      error: 'No token provided, must be set on the Authorization Header' });
   }
-}
-function generateToken(user) {
-  const payload = {
-    username: user.username
-  };
-  const options = {
-    expiresIn: '1d'
-  }
-  return jwt.sign(payload, secret.jwtKey, options);
 }
